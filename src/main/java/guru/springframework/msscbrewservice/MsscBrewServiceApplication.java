ff19981b512692bb5cbeaee9329d1903ae727a8a
SercanBesirli <sercanbesirli@gmail.com>
2020-12-26 15:09:42 +0300

    initial project. Closes #1

---
+++ src/main/java/guru/springframework/msscbrewservice/MsscBrewServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBrewServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/msscbrewservice/MsscBrewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
